75cf395834fbe575dc42c0efc147e6157e388d98
steofr <47863373+steofr@users.noreply.github.com>
2022-12-11 09:23:34 +0100

    JS

---
+++ JS/profil_gest.js
@@ -0,0 +1,83 @@
+$(document).ready(function(){
+    /* member space */
+     $("#consult_member_profil").hide();
+    $("#modify_member_profil").hide();
+    $("#delete_member_profil").hide();
+
+    $("#watch_consult_member_profil").click(function(){
+          $("#consult_member_profil").show();
+        });
+
+    $("#watch_modify_member_profil").click(function(){
+          $("#modify_member_profil").show();
+        });
+
+    $("#watch_delete_member_profil").click(function(){
+          $("#delete_member_profil").show();
+        });
+
+    $("#hide_consult_member_profil").click(function(){
+          $("#consult_member_profil").hide();
+        });
+
+    $("#hide_modify_member_profil").click(function(){
+          $("#modify_member_profil").hide();
+        });
+
+    $("#hide_delete_member_profil").click(function(){
+          $("#delete_member_profil").hide();
+        });
+
+    /* member space */
+
+    /* admin space */ 
+     $("#create_member_by_admin").hide();
+     $("#consult_member_by_admin").hide();
+     $("#modify_member_by_admin").hide();
+     $("#delete_member_by_admin").hide();
+    
+    $("#watch_create_member_by_admin").click(function(){
+          $("#create_member_by_admin").show();
+        });
+
+    $("#hide_create_member_by_admin").click(function(){
+          $("#create_member_by_admin").hide();
+        });
+
+    $("#watch_consult_member_by_admin").click(function(){
+          $("#consult_member_by_admin").show();
+        });
+
+    $("#hide_consult_member_by_admin").click(function(){
+          $("#consult_member_by_admin").hide();
+        });
+
+    $("#watch_modify_member_by_admin").click(function(){
+          $("#modify_member_by_admin").show();
+        });
+
+    $("#hide_modify_member_by_admin").click(function(){
+          $("#modify_member_by_admin").hide();
+        });
+        
+    $("#watch_delete_member_by_admin").click(function(){
+          $("#delete_member_by_admin").show();
+        });
+
+    $("#hide_delete_member_by_admin").click(function(){
+          $("#delete_member_by_admin").hide();
+        });
+
+    $("#gest_member_profil").hide();
+
+    $("#watch_gest_member_profil").click(function(){
+          $("#gest_member_profil").show();
+        });
+
+    $("#hide_gest_member_profil").click(function(){
+          $("#gest_member_profil").hide();
+        });
+
+    
+    /* admin space */ 
+    });
